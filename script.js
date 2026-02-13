ca7350e95d52873e7c13aefe0093ed25d1ba0209
kisakitx <uyuuruyeu@gmail.com>
2026-02-13 01:40:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+let idioma = "pt";
+
+function addFilme(){
+
+  let nome = document.getElementById("nomeFilme").value;
+  let capa = document.getElementById("capa").files[0];
+  let video = document.getElementById("video").files[0];
+
+  if(!nome || !capa || !video){
+    alert("Preencha tudo!");
+    return;
+  }
+
+  let div = document.createElement("div");
+  div.className = "filme";
+
+  let img = document.createElement("img");
+  img.src = URL.createObjectURL(capa);
+
+  let p = document.createElement("p");
+  p.innerText = nome;
+
+  div.appendChild(img);
+  div.appendChild(p);
+
+  div.onclick = ()=>{
+    selecionar(div);
+    abrirPlayer(video);
+  }
+
+  document.getElementById("catalogo").appendChild(div);
+
+  document.getElementById("nomeFilme").value = "";
+}
+
+function selecionar(el){
+
+  document.querySelectorAll(".filme").forEach(f=>{
+    f.classList.remove("ativo");
+  });
+
+  el.classList.add("ativo");
+}
+
+function abrirPlayer(video){
+
+  let player = document.getElementById("player");
+  player.src = URL.createObjectURL(video);
+
+  document.getElementById("playerBox").style.display = "flex";
+}
+
+function fecharPlayer(){
+  document.getElementById("playerBox").style.display = "none";
+}
+
+function toggleIdioma(){
+
+  let box = document.getElementById("idiomaBox");
+
+  if(box.style.display=="block"){
+    box.style.display="none";
+  }else{
+    box.style.display="block";
+  }
+}
+
+function setIdioma(lang){
+
+  idioma = lang;
+
+  alert("Idioma: " + (lang=="pt"?"Português":"Inglês"));
+
+  document.getElementById("idiomaBox").style.display="none";
+}
+
+/* CONFIG */
+
+function abrirConfig(){
+  document.getElementById("configBox").style.display="flex";
+}
+
+function fecharConfig(){
+  document.getElementById("configBox").style.display="none";
+}
+
+function trocarFundo(){
+
+  let img = document.getElementById("fundo").files[0];
+
+  if(!img){
+    alert("Escolha uma imagem");
+    return;
+  }
+
+  document.body.style.backgroundImage =
+    `url(${URL.createObjectURL(img)})`;
+}
